7e4fcc36bc98cf5deedebe5e448967c64203fd56
Sandeep Repaka <rsandeep86@gmail.com>
2015-11-16 23:31:32 -0800

    Add unit values to seeds.rbw

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,13 @@ OrderStatus.create! id: 3, status: "Picked Up"
 OrderStatus.create! id: 4, status: "Delivered"
 OrderStatus.create! id: 5, status: "Cancelled"
 
-
+Unit.delete_all
+Unit.create! id: 1, name: "ea"
+Unit.create! id: 2, name: "100g"
+Unit.create! id: 3, name: "250g"
+Unit.create! id: 4, name: "500g"
+Unit.create! id: 5, name: "1kg"
+Unit.create! id: 6, name: "100ml"
+Unit.create! id: 7, name: "250ml"
+Unit.create! id: 8, name: "500ml"
+Unit.create! id: 9, name: "1L"
